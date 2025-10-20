a2ac3351fe016cf7defab8e376522cf3cbee8be1
tab <tab@0x09.net>
2025-10-20 14:19:38 -0400

    magickwand.h: add descriptive heading

--- include/magickwand.h
+++ include/magickwand.h
@@ -1,3 +1,7 @@
+/*
+ * magickwand.h - Preprocessor #include wrapper for compatibility between ImageMagick versions 6 and 7.
+ */
+
 #if MAGICKWAND_VERSION > 6
 #include <MagickWand/MagickWand.h>
 #else
